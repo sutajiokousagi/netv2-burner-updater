653f98c99063d742e33d612c052c9ad0da0eb2e8
bunnie <bunnie@kosagi.com>
2018-11-14 16:14:31 +0800

    update to the updater for the quick test

--- update_quick_tester.sh
+++ update_quick_tester.sh
@@ -20,6 +20,23 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+printf "Building & installing quicktest voltage driver\n"
+cd /home/pi/code/netv2-tests/bin/testhat-drivers
+sudo rm -f tester-driver
+make
+sudo chown root tester-driver
+sudo chmod u+s tester-driver
+/home/pi/code/netv2-tests/bin/testhat-drivers/tester-driver -v
+
+printf "Updating the tester - UI\n"
+cd /home/pi/code/jig-20-interface-http
+git pull origin master
+if [ $? -ne 0 ]; then
+    printf "Trouble updating the tester, send this info to bunnie@alphamaxmedia.com\n"
+    read dummy
+    exit 1
+fi
+
 printf "Updating the tester images\n"
 cd /home/pi/code/netv2-tests/bin/netv2-fpga
 git pull origin tester-master
